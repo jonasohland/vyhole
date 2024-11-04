f31caf3083e355d67f93d2296316ba850cecbd6b
Jonas Ohland <jonas.ohland@gmail.com>
2024-11-04 16:34:20 +0100

    initial

--- pdns-block.lua
+++ pdns-block.lua
@@ -1,14 +1,14 @@
 block = newDS()
 allow = newDS()
 
+deny:add(dofile("/config/vyhole/out/deny.lua"))
+allow:add(dofile("/config/vyhole/out/allow.lua"))
+
 function preresolve(dq)
-	if allow:check(dq.qname) or (not block:check(dq.qname)) then
+	if allow:check(dq.qname) or (not deny:check(dq.qname)) then
 		return false
 	end
 
 	dq.rcode = pdns.NXDOMAIN
 	return true
 end
-
-block:add(dofile("/config/vyhole/out/block.lua"))
-allow:add(dofile("/config/vyhole/out/allow.lua"))
