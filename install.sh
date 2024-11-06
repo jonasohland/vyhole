51caae15fae099d7f0912355ad6a6bebd593ab07
Jonas Ohland <jonas.ohland@gmail.com>
2024-11-06 02:15:26 +0100

    ssh and gitconfig symlinks

--- install.sh
+++ install.sh
@@ -1,20 +1,49 @@
 #! /bin/bash
+
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 
 set -e
 
+function try_move_id() {
+    id_type="${1}"
+
+    id_file="/home/vyos/.ssh/id_${id_type}"
+    id_file_pub="/home/vyos/.ssh/id_${id_type}.pub"
+
+    if [[ -f "${id_file}" ]] && ! test -L "${id_file}"; then
+        mv -v "${id_file}" "/config/vyos/ssh/id_${id_type}"
+    fi
+
+    if [[ -f "${id_file_pub}" ]] && ! test -L "${id_file_pub}"; then
+        mv -v "${id_file_pub}" "/config/vyos/ssh/id_${id_type}.pub"
+    fi
+}
+
+mkdir -p /config/vyos/ssh
+
+try_move_id rsa
+try_move_id ecdsa
+try_move_id ed25519
+
+if [[ -f "/home/vyos/.gitconfig" ]] && ! test -L "/home/vyos/.gitconfig"; then
+    mv -v "/home/vyos/.gitconfig" "/home/vyos/gitconfig"
+fi
+
 mkdir -p /config/scripts/commit/post-hooks.d
+
 cp -v "${SCRIPT_DIR}/10-pdns-adblock-install" /config/scripts/commit/post-hooks.d/
+cp -v "${SCRIPT_DIR}/vyos-postconfig-bootup.script" /config/scripts/
 
 mkdir -p out
 
 if [[ ! -f "${SCRIPT_DIR}/config.txt" ]]; then
-  echo "allow_out /config/vyhole/out/allow.lua" >>"${SCRIPT_DIR}/config.txt"
-  echo "deny_out /config/vyhole/out/deny.lua" >>"${SCRIPT_DIR}/config.txt"
+    echo "allow_out /config/vyhole/out/allow.lua" >>"${SCRIPT_DIR}/config.txt"
+    echo "deny_out /config/vyhole/out/deny.lua" >>"${SCRIPT_DIR}/config.txt"
 
-  mkdir -p /config/vyhole/out
+    mkdir -p /config/vyhole/out
 fi
 
 "${SCRIPT_DIR}/update.sh" "${SCRIPT_DIR}/config.txt"
 
+/config/scripts/commit/vyos-postconfig-bootup.script
 /config/scripts/commit/post-hooks.d/10-pdns-adblock-install
